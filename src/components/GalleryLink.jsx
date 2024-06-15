4e361f52b8f85557c99689e1c58879ad45fc08b6
Diego Bravo A <digobravo@outlook.com>
2024-06-15 14:41:36 -0500

    added button

--- src/components/GalleryLink.jsx
+++ src/components/GalleryLink.jsx
@@ -1,10 +1,11 @@
-
 function GalleryLink() {
   return (
-    <div className="bg-gallery-image bg-contain h-96">       
-    GalleryLink
+    <div className="flex items-center justify-center bg-gallery-image bg-contain h-96 pt-52">
+        <button className="mx-auto border border-oldrose bg-oldrose bg-opacity-50 hover:bg-opacity-100 text-whitesmoke py-2 px-4 rounded ">
+          Galería
+        </button>
     </div>
-  )
+  );
 }
 
-export default GalleryLink
+export default GalleryLink;
