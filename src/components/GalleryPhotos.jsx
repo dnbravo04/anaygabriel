aabd81f91aab36026eea2cda2d68e82c94d23a42
Diego Bravo A <digobravo@outlook.com>
2024-07-13 20:13:29 -0500

    fixed images

--- src/components/GalleryPhotos.jsx
+++ src/components/GalleryPhotos.jsx
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Footer from "./Footer.jsx";
-import PhotoData from "../img/photos/photos.json";
 
+const imageBasePath = "../img/photos/jpeg_images/"; // Base path for image URLs
 
 export const GalleryPhotos = () => {
   const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
-    const importPhotos = async () => {
-      const importedPhotos = await Promise.all(
-        PhotoData.map(async (photo) => {
-          const src = await import(`../img/photos/${photo.src}`);
-          return { id: photo.id, src: src.default };
-        })
-      );
-      setPhotos(importedPhotos);
+    const importPhotos = () => {
+      const imageUrls = [];
+      for (let i = 1; i <= 33; i++) {
+        // Generate image URLs for 33 images
+        imageUrls.push(`${imageBasePath}image${i}.jpg`); // Adjust extension if needed
+      }
+      setPhotos(imageUrls); // Set the array of image URLs
     };
 
     importPhotos();
@@ -24,16 +23,20 @@ export const GalleryPhotos = () => {
   return (
     <>
       <div className="columns-2 md:columns-2 lg:columns-3 py-10 md:py-20 gap-4 mx-4">
-        {photos.map((photo) => (
+        {photos.map((imageUrl, index) => (
           <img
+            key={index} // Add a unique key for each image
             className="w-full mb-4 break-inside-avoid"
-            src={photo.src}
-            alt={`Image ${photo.id}`}
+            src={imageUrl}
+            alt={`Image ${index + 1}`} // Use index for alt text
           />
         ))}
       </div>
       <div className="my-4 text-center">
-        <Link to={-1} className="flex w-3/4 items-center justify-center mx-auto border font-semibold border-auburn bg-oldrose bg-opacity-50 hover:bg-opacity-70 text-whitesmoke py-2 px-8">
+        <Link
+          to={-1}
+          className="flex w-3/4 items-center justify-center mx-auto border font-semibold border-auburn bg-oldrose bg-opacity-50 hover:bg-opacity-70 text-whitesmoke py-2 px-8"
+        >
           Volver a la pagina principal
         </Link>
       </div>
