5ab2286ad305a53d1297827bce69ac4fe040384b
Diego Bravo A <digobravo@outlook.com>
2024-07-20 08:22:49 -0500

    Fixed (?) button text

--- src/components/MainFinalPart.jsx
+++ src/components/MainFinalPart.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function MainFinalPart() {
   return (
     <>
-        <div className="items-center bg-final-image bg-cover bg-no-repeat w-[320px] max-h-3/4 pt-72 flex mx-auto">
-      <a href="https://forms.gle/HQydLEpJQhRNys4i8" target="_blank" className="flex items-center mx-auto">
+        <div className="bg-final-image bg-cover bg-no-repeat w-[320px] max-h-3/4 pt-72 flex mx-auto">
+      <a href="https://forms.gle/HQydLEpJQhRNys4i8" target="_blank" className="flex mx-auto">
           <button
             className="
-          flex w-3/4 mx-auto border mb-5 border-auburn bg-oldrose bg-opacity-50 hover:bg-opacity-70 text-whitesmoke py-2 px-2"
+          w-3/4 mx-auto border mb-5 border-auburn bg-oldrose bg-opacity-50 hover:bg-opacity-70 text-whitesmoke py-2 px-2"
           >
             Responde a la invitación
           </button>
