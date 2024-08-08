c402a6c75e63a86fe848fca6a0dc7d5d47b8853f
Diego Bravo A <digobravo@outlook.com>
2024-08-07 21:16:55 -0500

    Changed icon and removed link to images from bigscreencomponent

--- src/components/BigScreenComponent.jsx
+++ src/components/BigScreenComponent.jsx
@@ -18,11 +18,6 @@ export const BigScreenComponent = () => {
           <h2 className="text-whitesmoke font-bold font-handlee pt-5 flex items-center justify-center mx-auto text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
             10.01.2025
           </h2>
-          <Link to={"/anaygabriel/galeria"}>
-            <p className="text-whitesmoke sm:mt-4 md:mt-5 lg:mt-20 font-handlee pt-5 text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-center underline">
-              Fotografías
-            </p>
-          </Link>{" "}
           <p className="text-whitesmoke sm:mt-4 md:mt-5 lg:mt-20 font-handlee pt-5 text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-center">
             Para ver la invitación y <br />
             confirmar tu asistencia utiliza tu teléfono
