225fcba3519c8aaf91337dced96e30926c928451
Diego Bravo A. <digobravo@outlook.com>
2024-09-30 11:15:13 -0500

    Update MainGalleryLink.jsx

--- src/components/MainGalleryLink.jsx
+++ src/components/MainGalleryLink.jsx
@@ -4,7 +4,7 @@ function GalleryLink() {
     <div className="items-center justify-center bg-gallery-image bg-cover bg-no-repeat w-full h-96 pt-72">
 
         <Link to={"/anaygabriel/galeria"}>
-          <div className="flex w-3/4 items-center justify-center mx-auto border font-semibold border-auburn bg-oldrose bg-opacity-50 hover:bg-opacity-70 text-whitesmoke py-2 px-8">
+          <div className="flex w-3/4 items-center justify-center mx-auto border font-semibold border-auburn bg-oldrose hover:bg-opacity-70 text-whitesmoke py-2 px-8">
             Galería
           </div>
         </Link>
